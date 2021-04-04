8696075c0a1f585fe36b699bca6bc275b87223d7
shiftinv <me@shiftinv.cc>
2021-04-04 13:59:37 +0200

    Fix AttributeRawCopy context key naming

--- constructutils/rawcopy.py
+++ constructutils/rawcopy.py
@@ -1,5 +1,5 @@
 from construct import Struct, Array, RawCopy
-from typing import Union
+from typing import Optional, Union, overload
 
 
 class RawCopyError(Exception):
@@ -9,7 +9,7 @@ class RawCopyError(Exception):
 class AttributeRawCopy(RawCopy):
     '''
     Similar to :class:`RawCopy`, but instead of returning a dict `{'data': [bytes], 'value': [Any]}`
-    it assigns the raw `data` to a property of `value` (`__raw__` by default, can be changed with `@`).
+    it assigns the raw `data` to a property of `value` (`__raw__` by default, can be changed with `@` or using the parameters).
 
     Especially useful in combination with :class:`inline.InlineStruct`.
 
@@ -18,20 +18,25 @@ class AttributeRawCopy(RawCopy):
         ...     'val' / Byte,
         ...     'raw_data' @ AttributeRawCopy(InlineStruct(
         ...         'num' / Int16ul
-        ...     ))
+        ...     )),
+        ...     'array' / AttributeRawCopy(Array(3, Byte))
         ... )
         ...
-        >>> c = s.parse(b'\\xff\\x00\\x02')
-        >>> assert c.val == 0xff and c.num == 0x200
+        >>> c = s.parse(b'\\xff\\x00\\x02\\x0a\\x0b\\x0c')
+        >>> assert c.val == 0xff and c.num == 0x200 and c.array == [10, 11, 12]
         >>> assert c.raw_data == b'\\x00\\x02'
+        >>> assert c.array.__raw__ == b'\\x0a\\x0b\\x0c'
     '''
 
     __raw_key = '__raw__'
 
-    def __init__(self, subcon: Union[Struct, Array]):
+    def __init__(self, subcon: Union[Struct, Array], raw_key: Optional[str] = None):
         if not isinstance(subcon, (Struct, Array)):
             raise RawCopyError('AttributeRawCopy must contain `Struct` or `Array` instance')
         super().__init__(subcon)
+
+        if raw_key is not None:
+            self.__raw_key = raw_key
 
     def _parse(self, stream, context, path):
         rc = super()._parse(stream, context, path)
@@ -48,5 +53,4 @@ class AttributeRawCopy(RawCopy):
         return super()._build({'value': obj}, stream, context, path)
 
     def __rmatmul__(self, name):
-        self.__raw_key = name
-        return self
+        return type(self)(self.subcon, name)
